cd5f98de9fccf2cfeb0e4b3431e934bad70600c8
WooKyoung Noh <wookay.noh@gmail.com>
2016-08-25 22:02:19 +0900

    update welcome

--- welcome.jl
+++ welcome.jl
@@ -6,30 +6,37 @@ end
 function layout(::Layout, body, options)
     """
 <html>
-  <head><title>Bukdu</title></head>
+  <head><title>Bukdu 🌌 </title></head>
   <body>$body<body>
 </html>
 """
 end
 
 function index(::WelcomeController)
+    repo = LibGit2.GitRepo(Pkg.dir("Bukdu"))
+    commit = string(LibGit2.revparseid(repo, "HEAD"))
     text = """
 # Bukdu 🌌
 
-[https://github.com/wookay/Bukdu.jl](https://github.com/wookay/Bukdu.jl)
-
-Bukdu is a web development framework for [Julia](http://julialang.org).
+[Bukdu](https://github.com/wookay/Bukdu.jl) is a web development framework for [Julia](http://julialang.org).
 
 It's influenced by [Phoenix framework](http://phoenixframework.org).
 
 
 # Bukdu on Heroku
 
-You can run Bukdu on Heroku.
+You can run Bukdu on [Heroku](https://www.heroku.com/).
 
-[https://github.com/wookay/heroku-bukdu](https://github.com/wookay/heroku-bukdu)
+ * [heroku-bukdu](https://github.com/wookay/heroku-bukdu)
 
-[https://github.com/wookay/heroku-buildpack-julia](https://github.com/wookay/heroku-buildpack-julia)
+ * [heroku-buildpack-julia](https://github.com/wookay/heroku-buildpack-julia)
+
+
+# Welcome
+
+This page is running on Bukdu commit [$commit](https://github.com/wookay/Bukdu.jl/commit/$commit) in Juila $(VERSION)
+
+See the full code at [welcome.jl](https://github.com/wookay/heroku-bukdu/blob/master/welcome.jl).
 """
     render(Markdown/Layout, text)
 end
